84eb0831701bdf1476219717d2eb4febba64b310
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-10-30 01:50:25 +0200

    Set fixed navbar

--- nextjs/components/navbar.tsx
+++ nextjs/components/navbar.tsx
@@ -1,6 +1,6 @@
 import type { NextComponentType } from 'next';
 import Link from 'next/link';
-import { Disclosure} from '@headlessui/react';
+import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { navMenus } from '../fixtures/navbar.fixttures';
 import { useRouter } from 'next/router'
@@ -11,7 +11,7 @@ const Navbar: NextComponentType = () => {
   const router = useRouter()
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800 fixed w-full z-10 top-0">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
